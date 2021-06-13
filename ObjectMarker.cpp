1acf4da10275ca46df05cb0929f7002aedd1ad8d
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2021-06-13 19:48:03 +0000

    0.0.12

--- ObjectMarker.cpp
+++ ObjectMarker.cpp
@@ -1,3 +1,57 @@
 
 /*M///////////////////////////////////////////////////////////////////////////////////////
 //
+//  IMPORTANT: READ BEFORE DOWNLOADING, COPYING, INSTALLING OR USING.
+//
+//  By downloading, copying, installing or using the software you agree to this license.
+//  If you do not agree to this license, do not download, install,
+//  copy or use the software.
+//
+//
+//                           License Agreement
+//
+// Copyright (C) 2014 Takuya MINAGAWA, 2006 Gunawan Herman, 2003 Florian Adolf.
+// Third party copyrights are property of their respective owners.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//M*/
+#include "ObjectMarker.h"
+#include "util_functions.h"
+#include "util_cv_functions.h"
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <boost/filesystem/path.hpp>
+#include <iostream>
+
+using namespace std;
+
+ObjectMarker::ObjectMarker(){
+}
+
+
+ObjectMarker::~ObjectMarker(void){
+}
+
+
+
+bool ObjectMarker::saveConfiguration(const std::string& config_name,
+	const std::string& input_dir, const std::string& outputname,
+	const MarkerViewer& marker_viewer)
+{
+	cv::FileStorage fs(config_name, cv::FileStorage::WRITE);
