e599f7459edd7ee2b91b4f8ff69ddada3016b5cc
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2021-06-15 22:39:21 +0000

    log in

--- ObjectMarker.cpp
+++ ObjectMarker.cpp
@@ -108,3 +108,64 @@ void ObjectMarker::printHelp() {
 	printf("|7      | 一番新しいマーカーを10px右へ動かす               |\n");
 	printf("|w      | 一番新しいマーカーの幅を1 px拡大する             |\n");
 	printf("|W      | 一番新しいマーカーの幅を1 px縮小する             |\n");
+	printf("|h      | 一番新しいマーカーの高さを1 px拡大する           |\n");
+	printf("|H      | 一番新しいマーカーの高さを1 px縮小する           |\n");
+	printf("|z      | 一番新しいマーカーのを大きさを2%%拡大する         |\n");
+	printf("|Z      | 一番新しいマーカーのを大きさを2%%縮小する         |\n");
+	printf("|m      | マーカーの縦横比を固定／固定解除する             |\n");
+	printf("|a      | マーカーの縦横比を指定する                       |\n");
+	printf("|s      | 画像の表示サイズを変更する                       |\n");
+	printf("|c      | マーカー領域の画像を切り取ってファイル保存       |\n");
+	printf("|p      | マーカーを矩形だけでなく点を許可する             |\n");
+	printf("|e      | 現フレームにつけたマーカー座標を画面出力         |\n");
+	printf("|g      | ガイドの表示/非表示                              |\n");
+	printf("|G      | ガイドの設定                                     |\n");
+	printf("|f      | 画像のフォルダ名を変更                           |\n");
+	printf("|o      | 出力ファイルを変更                               |\n");
+	printf("|O      | 出力ファイルを成形して新たに作成                 |\n");
+	printf("|j      | 指定番号の画像へジャンプ                         |\n");
+	printf("|t      | このヘルプを表示                                 |\n");
+	printf("------------------------------------------------------------\n");
+	printf("オブジェクトを右クリックで選択\n");
+	printf("\n");
+}
+
+void ObjectMarker::printStatus() const
+{
+	std::cout << "このプログラムは入力画像を'" << _input_dir << "'フォルダの中から探します\n";
+	std::cout << "オブジェクトの位置はファイル'" << _annotation_file << "'にテキスト出力されます\n";
+	_marker_viewer.PrintStatus();
+}
+
+
+//! アノテーションの並び替え
+/*!
+ロードされたアノテーションに対応する画像ファイル名が、参照画像ファイル名と対応するように並び替え
+\param[in] loaded_img_list 入力画像リスト
+\param[in] loaded_annotation 読み込まれたアノテーション
+\param[in] ref_img_list 参照アノテーション
+\return 整理されたアノテーション
+*/
+std::vector<std::vector<cv::Rect>> ObjectMarker::reorderAnnotation(
+	const std::vector<std::string>& loaded_img_list,
+	const std::vector<std::vector<cv::Rect>>& loaded_annotation,
+	const std::vector<std::string>& ref_img_list)
+{
+	assert(loaded_img_list.size() == loaded_annotation.size());
+	using namespace boost::filesystem;
+
+	std::vector<std::vector<cv::Rect>> annotation(ref_img_list.size());
+
+	int num_loaded = loaded_img_list.size();
+	int num_ref = ref_img_list.size();
+
+	int i,j;
+	for (i = 0; i < num_loaded; i++){
+		path loaded_img_path = path(loaded_img_list[i]);
+		for (j = 0; j < num_ref; j++){
+			if (loaded_img_path.compare(ref_img_list[j]) == 0)
+				break;
+		}
+
+		if (j < num_ref)
+			annotation[j] = loaded_annotation[i];
