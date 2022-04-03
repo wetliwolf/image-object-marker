6e8c1393f8eaa1dec5db7eb0e5496381928b3591
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2022-04-03 19:58:35 +0000

    build error

--- ReadCSVFile.hpp
+++ ReadCSVFile.hpp
@@ -13,4 +13,5 @@
 // Third party copyrights are property of their respective owners.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restricti
