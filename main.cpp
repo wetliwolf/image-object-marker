401cb5a645bad06f9f2d0bd1f931f23ccb03828b
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2022-12-08 09:41:00 +0000

    Increase version number

--- main.cpp
+++ main.cpp
@@ -20,4 +20,6 @@
 // so, subject to the following conditions:
 //
 // The above copyright notice and this permission notice shall be included in all
-// copies or substan
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDE
