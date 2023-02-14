6e071210dc8120ce6f38cf705861fbb306b387f1
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2023-02-14 10:12:31 +0000

    Update fetch-steamos.sh

--- main.cpp
+++ main.cpp
@@ -39,4 +39,5 @@ int main(int argc, char* argv[])
 	std::string config_file = (argc > 1) ? argv[1] : "config.xml";
 	ObjectMarker object_marker;
 
-	obje
+	object_marker.run(config_file);
+}
---
+++ util_cv_functions.cpp
@@ -0,0 +1,20 @@
+
+/*M///////////////////////////////////////////////////////////////////////////////////////
+//
+//  IMPORTANT: READ BEFORE DOWNLOADING, COPYING, INSTALLING OR USING.
+//
+//  By downloading, copying, installing or using the software you agree to this license.
+//  If you do not agree to this license, do not download, install,
+//  copy or use the software.
+//
+//
+//                           License Agreement
+//
+// Copyright (C) 2014 Takuya MINAGAWA, 2006 Gunawan Herman, 2003 Florian Adolf.
+// Third party copyrights are property of their respective owners.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is furnished to do
