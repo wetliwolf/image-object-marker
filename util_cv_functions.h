c7d8a76f5d1e37918d00519ac8c21d216b921581
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2023-02-27 07:32:31 +0000

    a word

--- util_cv_functions.h
+++ util_cv_functions.h
@@ -2,3 +2,58 @@
 /*M///////////////////////////////////////////////////////////////////////////////////////
 //
 //  IMPORTANT: READ BEFORE DOWNLOADING, COPYING, INSTALLING OR USING.
+//
+//  By downloading, copying, installing or using the software you agree to this license.
+//  If you do not agree to this license, do not download, install,
+//  copy or use the software.
+//
+//
+//                           License Agreement
+//
+// Copyright (C) 2014 Takuya MINAGAWA, 2006 Gunawan Herman, 2003 Florian Adolf.
+// Third party copyrights are property of their respective owners.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//M*/
+
+#ifndef __UTIL_CV_FUNCTIONS__
+#define __UTIL_CV_FUNCTIONS__
+
+#include <opencv2/core/core.hpp>
+
+namespace util{
+	
+	//! アノテーションファイルの読み込み
+	/*!
+	opencv_createsamles.exeと同形式のアノテーションファイル読み書き
+	\param[in] gt_file アノテーションファイル名
+	\param[out] imgpathlist 画像ファイルへのパス
+	\param[out] rectlist 各画像につけられたアノテーションのリスト
+	\return 読み込みの成否
+	*/
+	bool LoadAnnotationFile(const std::string& gt_file, std::vector<std::string>& imgpathlist, std::vector<std::vector<cv::Rect>>& rectlist);
+
+	//! アノテーションファイルの保存
+	/*!
+	opencv_createsamles.exeと同形式のアノテーションファイル読み書き
+	\param[in] anno_file アノテーションファイル名
+	\param[in] img_files 画像ファイルへのパス
+	\param[in] obj_rects 各画像につけられたアノテーションのリスト
+	\return 保存の成否
+	*/
