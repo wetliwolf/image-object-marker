3fc37f15b31c2498ee1745c10eb72cd00f9985e0
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2023-06-01 06:47:03 +0000

    fix: lint errors

--- util_functions.cpp
+++ util_functions.cpp
@@ -13,4 +13,4 @@
 // Third party copyrights are property of their respective owners.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
-// 
+// of this software and associated documentation files (the
