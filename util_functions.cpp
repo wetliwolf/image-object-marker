68215ab8dfc32408bff7830ebdb4aa2505e1bc9c
wetliwolf <180749839+wetliwolf@users.noreply.github.com>
2024-07-22 21:59:05 +0000

    make jshint happy

--- util_functions.cpp
+++ util_functions.cpp
@@ -72,4 +72,4 @@ namespace util{
 		for (directory_iterator p(img_dir_path); p != end; ++p){
 			path file_p = p->path();
 			std::string ext = file_p.extension().string();
-			if (ext == ".jpg" || ext == ".jpeg" 
+			if (ext == ".jpg" || ext == ".jpeg" || ext == ".JPG" || ext == ".JPEG" || ext == ".bmp" || ex
